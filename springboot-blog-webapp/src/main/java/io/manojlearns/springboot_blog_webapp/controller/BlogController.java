0796efafe94b637381a65eae024e10c9cae02026
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2025-02-16 18:55:15 -0500

    added client side - view each post

--- springboot-blog-webapp/src/main/java/io/manojlearns/springboot_blog_webapp/controller/BlogController.java
+++ springboot-blog-webapp/src/main/java/io/manojlearns/springboot_blog_webapp/controller/BlogController.java
@@ -5,6 +5,8 @@ import io.manojlearns.springboot_blog_webapp.dto.PostDto;
 import io.manojlearns.springboot_blog_webapp.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -19,10 +21,19 @@ public class BlogController {
         this.postService = postService;
     }
 
+    //handler method to handle http://localhost:8080/
     @RequestMapping("/")
     public String blogPosts(Model model){
         List<PostDto> posts = postService.findAllPosts();
         model.addAttribute("postsResponse", posts);
         return "blog/view_posts";
+    }
+
+    //handler method to handle view post request
+    @GetMapping("post/{postUrl}")
+    public String showPost(@PathVariable("postUrl") String postUrl, Model model){
+        PostDto post = postService.findPostByUrl(postUrl);
+        model.addAttribute("post", post);
+        return "blog/blog_post";
     }
 }
