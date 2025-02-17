7cbd53da0fa6ae5a6f857fbd002ee9c9600e3a47
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2025-02-16 19:23:32 -0500

    added search option

--- springboot-blog-webapp/src/main/java/io/manojlearns/springboot_blog_webapp/controller/BlogController.java
+++ springboot-blog-webapp/src/main/java/io/manojlearns/springboot_blog_webapp/controller/BlogController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 import java.util.List;
@@ -35,5 +36,14 @@ public class BlogController {
         PostDto post = postService.findPostByUrl(postUrl);
         model.addAttribute("post", post);
         return "blog/blog_post";
+    }
+
+    //Handler method to handle blog post search request
+    //http://localhost:8080/page/search?query=java
+    @GetMapping("page/search")
+    public String searchPosts(@RequestParam(value="query") String query, Model model ){
+        List<PostDto> postsResponse = postService.searchPosts(query);
+        model.addAttribute("postsResponse", postsResponse);
+        return "blog/view_posts";
     }
 }
