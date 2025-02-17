e9b6c61553851c80db483bc9810885ab11b4a967
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2025-02-17 10:53:49 -0500

    introducing comments entity

--- springboot-blog-webapp/src/main/java/io/manojlearns/springboot_blog_webapp/entity/Comment.java
+++ springboot-blog-webapp/src/main/java/io/manojlearns/springboot_blog_webapp/entity/Comment.java
@@ -4,42 +4,37 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 @Entity
-@Builder
-@Table(name = "posts")
-public class Post {
+@Table(name="comments")
+public class Comment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(nullable = false)
-    private String title;
-
-    private String url;
-
-//    @Lob
+    private String name;
+    @Column(nullable = false)
+    private String email;
     @Column(nullable = false, columnDefinition="LONGTEXT")
     private String content;
-
-    private String shortDescription;
-
     @CreationTimestamp
     private LocalDateTime createdOn;
-
     @UpdateTimestamp
     private LocalDateTime updatedOn;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
-    private Set<Comment> comments= new HashSet<>();
-
+    @ManyToOne
+    @JoinColumn(name="post_id", nullable=false)
+    private Post post;
 
 }
